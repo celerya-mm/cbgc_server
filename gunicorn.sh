6758314b44e255a000f2c7b7bc9f711d20d8ec21
celerya-mm <mazzini@celerya.com>
2023-02-02 23:14:13 +0100

    setup nginx with ssl 2

--- gunicorn.sh
+++ gunicorn.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 #echo Starting app...
-gunicorn -w 2 --threads 2 --log-config gunicorn/gunicorn_logging.conf -b 0.0.0.0:5000 --reload wsgi:app
+gunicorn -w 4 --threads 4 --log-config gunicorn/gunicorn_logging.conf -b 0.0.0.0:5000 --reload wsgi:app
